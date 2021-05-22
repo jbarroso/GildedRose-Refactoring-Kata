e2d52d8466218d65296ae169ba0b2ee200f0fe72
Jose Ángel Barroso <jbarroso@antevenio.com>
2021-05-22 12:04:50 +0200

    Adding test: should decrease quality by one with normal item

--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -23,4 +23,20 @@ class GildedRoseTest extends TestCase
         // then
         $this->assertSame($sellIn - 1, $item->sell_in);
     }
+
+    public function testShouldDecreaseQualityByOneWithNormalItem(): void
+    {
+        // given
+        $sellIn = 2;
+        $quality = 3;
+        $item = new Item('foo', $sellIn, $quality);
+
+        // when
+        $gildedRose = new GildedRose([$item]);
+        $gildedRose->updateQuality();
+
+        // then
+        $this->assertSame($quality - 1, $item->quality);
+    }
+
 }
