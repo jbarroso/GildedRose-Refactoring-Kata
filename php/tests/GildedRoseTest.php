251d6ae31e2fd92f57894b74eb85829a887a5659
Jose Ángel Barroso <jbarroso@antevenio.com>
2021-05-22 12:25:25 +0200

    Refactoring test: adding item to property class

--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -13,31 +13,32 @@ class GildedRoseTest extends TestCase
     const POSITIVE_SELL_IN = 2;
     const POSITIVE_QUALITY = 3;
     const NORMAL_ITEM = 'foo';
+    private Item $item;
 
     public function testShouldDecreaseSellInByOneWithNormalItem(): void
     {
         // given
-        $item = new Item(self::NORMAL_ITEM, self::POSITIVE_SELL_IN, self::POSITIVE_QUALITY);
+        $this->item = new Item(self::NORMAL_ITEM, self::POSITIVE_SELL_IN, self::POSITIVE_QUALITY);
 
         // when
-        $gildedRose = new GildedRose([$item]);
+        $gildedRose = new GildedRose([$this->item]);
         $gildedRose->updateQuality();
 
         // then
-        $this->assertSame(self::POSITIVE_SELL_IN - 1, $item->sell_in);
+        $this->assertSame(self::POSITIVE_SELL_IN - 1, $this->item->sell_in);
     }
 
     public function testShouldDecreaseQualityByOneWithNormalItem(): void
     {
         // given
-        $item = new Item(self::NORMAL_ITEM, self::POSITIVE_SELL_IN, self::POSITIVE_QUALITY);
+        $this->item = new Item(self::NORMAL_ITEM, self::POSITIVE_SELL_IN, self::POSITIVE_QUALITY);
 
         // when
-        $gildedRose = new GildedRose([$item]);
+        $gildedRose = new GildedRose([$this->item]);
         $gildedRose->updateQuality();
 
         // then
-        $this->assertSame(self::POSITIVE_QUALITY - 1, $item->quality);
+        $this->assertSame(self::POSITIVE_QUALITY - 1, $this->item->quality);
     }
 
 }
