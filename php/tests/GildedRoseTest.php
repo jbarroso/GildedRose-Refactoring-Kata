900120cb222f08a2dd8a0c188a9db7bc02b76faf
Jose Ángel Barroso <jbarroso@antevenio.com>
2021-05-22 12:54:37 +0200

    Refactoring test: extracting given/when/then methods

--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -17,28 +17,40 @@ class GildedRoseTest extends TestCase
 
     public function testShouldDecreaseSellInByOneWithNormalItem(): void
     {
-        // given
-        $this->item = new Item(self::NORMAL_ITEM, self::POSITIVE_SELL_IN, self::POSITIVE_QUALITY);
+        $this->givenItem(self::NORMAL_ITEM);
 
-        // when
-        $gildedRose = new GildedRose([$this->item]);
-        $gildedRose->updateQuality();
+        $this->whenUpdateQuality();
 
-        // then
-        $this->assertSame(self::POSITIVE_SELL_IN - 1, $this->item->sell_in);
+        $this->thenSellInIsEqualTo(self::POSITIVE_SELL_IN - 1);
     }
 
     public function testShouldDecreaseQualityByOneWithNormalItem(): void
     {
-        // given
-        $this->item = new Item(self::NORMAL_ITEM, self::POSITIVE_SELL_IN, self::POSITIVE_QUALITY);
+        $this->givenItem(self::NORMAL_ITEM);
 
-        // when
-        $gildedRose = new GildedRose([$this->item]);
-        $gildedRose->updateQuality();
+        $this->whenUpdateQuality();
 
-        // then
-        $this->assertSame(self::POSITIVE_QUALITY - 1, $this->item->quality);
+        $this->thenQualityIsEqualTo(self::POSITIVE_QUALITY - 1);
     }
 
+    private function givenItem(string $name): void
+    {
+        $this->item = new Item($name, self::POSITIVE_SELL_IN, self::POSITIVE_QUALITY);
+    }
+
+    private function whenUpdateQuality(): void
+    {
+        $gildedRose = new GildedRose([$this->item]);
+        $gildedRose->updateQuality();
+    }
+
+    private function thenSellInIsEqualTo(int $expectedSellIn): void
+    {
+        $this->assertSame($expectedSellIn, $this->item->sell_in);
+    }
+
+    private function thenQualityIsEqualTo(int $expectedQuality): void
+    {
+        $this->assertSame($expectedQuality, $this->item->quality);
+    }
 }
